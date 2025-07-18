d71004bc92ad28b67d23b9cb7feee92ba4ffdf46
Anas <jonsnow741852@gmail.com>
2025-07-18 16:37:25 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './alx-react-app/src/WelcomeMessage.jsx';
+import WelcomeMessage from './WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
