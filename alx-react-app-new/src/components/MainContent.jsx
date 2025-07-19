c1517280f813d9da9b432f23cbb267690262312b
Anas <jonsnow741852@gmail.com>
2025-07-19 16:04:21 +0100

    Update MainContent.jsx

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f8ff', padding: '20px', fontSize: '18px', color: '#333' }}>
+      <p>Welcome to my favorite cities application. Explore and enjoy!</p>
+    </main>
+  );
+}
+
+export default MainContent;
