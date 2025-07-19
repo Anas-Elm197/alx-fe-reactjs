2c4ad2770a671b33823374de34e484679812de44
Anas <jonsnow741852@gmail.com>
2025-07-19 17:06:30 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -8,6 +10,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };import React from 'react';
+  
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
 
 function App() {
   const [count, setCount] = useState(0)
